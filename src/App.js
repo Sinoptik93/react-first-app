bd89784296e20891d722f5263469ecf3cadb6412
Sinner93 <chepukhin.sergey@gmail.com>
2020-04-23 19:05:45 +0500

    12 step

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+import Header from './components/Header';
+import Profile from './components/Profile';
+import Navbar from './components/Navbar';
+
+const App = () => { 
+  return (
+    <div className='app-wrapper'>
+      <Header />
+      <Navbar />
+      <Profile />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+const Profile = () => {
+  return(
+  <div className='content'>
+    <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRajch10qkK_OsqMzQ8cgNV7XPF6US_zAtm4tEOELXqHO2_2meB&usqp=CAU' alt='img'></img>
+    <div>
+      ava + description
+    </div>
+    <div>
+      My post
+      <div>
+        New post
+      </div>
+      <div>
+        Post 1
+      </div>
+      <div>
+        Post 2
+      </div>
+    </div>
+  </div>)
+}
+
+export default Profile;
